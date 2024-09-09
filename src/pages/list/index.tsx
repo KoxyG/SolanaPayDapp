4f57632ef518c152c954b3551071d074f06e1990
koxy <devkoxy@gmail.com>
2024-09-09 08:26:54 +0100

    hero page

---
+++ src/pages/list/index.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+
+export default function List() {
+  return (
+    <div
+    >
+     LIST NOW
+    </div>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,8 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: "#fde9d8",
+        foreground: "",
       },
     },
   },
